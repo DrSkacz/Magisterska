42f9f07d4d7fbbf8db594329aa6d508b24d1475c
Skacz <Skacz@DESKTOP-QBJBNV9>
2020-08-12 18:42:13 +0200

    Dodaj pliki .gitignore i .gitattributes.

---
+++ Magisterka/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Magisterka.Models;
+
+namespace Magisterka.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Informacje o autorze programu.";
+
+            return View();
+        }
+
+        public IActionResult PoziomyKopec()
+        {
+            ViewData["Message"] = "Obliczenia długosci poziomego wymiennika ciepła wg. Kopcia";
+
+            return View();
+        }
+
+        public IActionResult PoziomyRubik()
+        {
+            ViewData["Message"] = "Obliczenia długosci poziomego wymiennika ciepła wg. Rubika";
+
+            return View();
+        }
+
+        public IActionResult Pionowy()
+        {
+            ViewData["Message"] = "Obliczenia długosci pionowego wymiennika ciepła";
+
+            return View();
+        }
+
+        public IActionResult Baza()
+        {
+            ViewData["Message"] = "Podgląd bazy danych";
+
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Magisterka/Models/Obl.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Magisterka.Models
+{
+    public class Obl
+    {
+        public int RQg { get; set; }
+
+        public int RFi { get; set; }
+
+        public int Rro { get; set; }
+
+        public int RQ0 { get; set; }
+
+        public int RXo { get; set; }
+
+        public int RDz { get; set; }
+
+        public int Rtinf { get; set; }
+
+        public int Rt { get; set; }
+
+        public int RL { get; set; }
+
+       
+    }
+}
